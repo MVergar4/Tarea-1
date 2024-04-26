be6d47a56396cb351907d8592115c9f84a048152
pcfuenr <panchocifuentes1234@gmail.com>
2024-04-26 13:58:44 -0400

    Prototipo deposito generico.

--- Expendedor/src/main/java/org/example/DepositoG.java
+++ Expendedor/src/main/java/org/example/DepositoG.java
@@ -1,6 +1,19 @@
 package org.example;
 import java.util.ArrayList;
 
-class DepositoG<T> {
-
+class DepositoG<T>{
+    private ArrayList<T> al;
+    public DepositoG(){
+        this.al = new ArrayList();
+    }
+    public Object getObject(){
+        if (al.size() > 0) {
+            return al.remove(0);
+        } else {
+            return null;
+        }
+    }
+    public Object addObject(Object obj){
+        al.add(obj);
+    }
 }
