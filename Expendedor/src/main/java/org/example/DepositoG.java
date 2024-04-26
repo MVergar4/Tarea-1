07f9654d6cd7a40738e6d112031d11a73fb9a8f5
pcfuenr <panchocifuentes1234@gmail.com>
2024-04-26 12:41:58 -0400

    corregido error en clase dulce

---
+++ Expendedor/src/main/java/org/example/DepositoG.java
@@ -0,0 +1,6 @@
+package org.example;
+import java.util.ArrayList;
+
+class DepositoG<T> {
+
+}
--- Expendedor/src/main/java/org/example/Dulce.java
+++ Expendedor/src/main/java/org/example/Dulce.java
@@ -1,6 +1,6 @@
 package org.example;
 
-class Dulce extends Producto{
-    public Dulce(int x){ super(x); }
+public abstract class Dulce extends Producto {
+    public Dulce(int x) { super(x); }
     public abstract String consumir();
 }
