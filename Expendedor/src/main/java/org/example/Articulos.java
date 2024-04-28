853e4d5fa0eda4a3f919c7230aebb4aed3d270f7
pcfuenr <panchocifuentes1234@gmail.com>
2024-04-28 17:14:54 -0400

    Documentacion clase articulos

--- Expendedor/src/main/java/org/example/Articulos.java
+++ Expendedor/src/main/java/org/example/Articulos.java
@@ -1,16 +1,18 @@
 package org.example;
-
+/**
+ * Un enum donde se guardan los tipos de productos y sirve para retornar los precios de estos mismos.
+ */
 public enum Articulos{
     COCA(900),
     SPRITE(800),
-    FANTA(600), //Precio de cada articulo.
+    FANTA(600),
     SNICKERS(400),
     SUPER8(300);
-    private final int precio;
-    Articulos(int x){
+    private final int precio; /** Entero para almacenar el precio del producto pedido */
+    Articulos(int x){ /** Metodo que guarda el precio del producto solicitado*/
         this.precio=x;
     }
     public int getPrecio(){
-        return precio;
+        return precio; /** Getter del precio del producto solicitado */
     }
 }
