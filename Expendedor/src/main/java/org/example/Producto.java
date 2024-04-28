32d3433c5c443963f4b41a35be93fdc0e59de509
pcfuenr <panchocifuentes1234@gmail.com>
2024-04-28 19:30:14 -0400

    documentada clase producto

--- Expendedor/src/main/java/org/example/Producto.java
+++ Expendedor/src/main/java/org/example/Producto.java
@@ -1,10 +1,15 @@
 package org.example;
 
+/**
+ * Superclase para todos los tipos de productos
+ */
 public abstract class Producto {
-    public Producto() {
-    }
+    /** Constructor */
+    public Producto() {}
+    /** Metodo que devuelve la direccion del objeto creado que corresponde a el numero de serie del producto.*/
     public Producto getSerie() {
         return this;
     }
+    /** Metodo abstracto que despues sirve para devolver que producto se ha consumido/devuelto */
     public abstract String consumir();
 }
